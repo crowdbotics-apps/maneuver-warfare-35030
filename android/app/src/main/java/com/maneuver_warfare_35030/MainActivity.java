b3ad695498ee5281e082792164fa45e7cd2feae9
Crowdbotics <team@crowdbotics.com>
2022-05-01 18:34:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/maneuver_warfare_35030/MainActivity.java
@@ -0,0 +1,15 @@
+package com.maneuver_warfare_35030;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "maneuver_warfare_35030";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "maneuver_warfare_35030",
+  url: "https://maneuver_warfare_35030.botics.co",
+  api: "https://maneuver_warfare_35030.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
